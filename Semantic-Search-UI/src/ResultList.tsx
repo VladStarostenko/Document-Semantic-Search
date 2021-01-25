5c6002065fe79c64a4bf7fe5b80fa79223748c97
Ktars <ktars1324@gmail.com>
2021-01-25 17:20:58 +0100

    added azure cognitive search

--- Semantic-Search-UI/src/ResultList.tsx
+++ Semantic-Search-UI/src/ResultList.tsx
@@ -3,8 +3,8 @@ import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Typography from "@material-ui/core/Typography";
-import {Link} from "@material-ui/core";
-
+import { Link } from "@material-ui/core";
+import "./index.css";
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -17,25 +17,40 @@ const useStyles = makeStyles((theme: Theme) =>
     title: {
       margin: theme.spacing(4, 0, 2),
     },
+    searchmatch: {
+      color: "red",
+    },
   })
 );
 
-
-export default ({result}:{result: Array<any>}) => {
+export default ({ result, total }: { result: Array<any>; total: number }) => {
   const classes = useStyles();
 
   return (
     <>
       <Typography variant="h6" className={classes.title}>
-        {result.length > 0 ? `${result.length} Results` : "Enter something ..."}
+        {result.length > 0
+          ? `Top ${result.length} results out of ${total}`
+          : "No results"}
       </Typography>
       <div className={classes.demo}>
         <List>
           {result.map((element: any) => (
-            <ListItem>
-              <Link href={element.link} rel={"noopener noreferrer"} target={"_blank"} >
-                {element.name}
+            <ListItem style={{ flexDirection: "column" }} key={element.id}>
+              <Link
+                href={element.link}
+                rel={"noopener noreferrer"}
+                target={"_blank"}
+              >
+                <h4>{element.name}</h4>
               </Link>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: element.snippet
+                    .replace(/<span class="searchmatch">/g, "<b>")
+                    .replace(/<\/span>/g, "</b>"),
+                }}
+              />
             </ListItem>
           ))}
         </List>
